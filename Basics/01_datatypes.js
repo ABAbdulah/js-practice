ffb19dddb746bf55494adab40cc0ff8fb06b156b
Abdullah Tahir <89691370+ABAbdulah@users.noreply.github.com>
2025-07-21 12:47:12 +0000

    variables

---
+++ Basics/01_datatypes.js
@@ -0,0 +1,6 @@
+"use strict" ; //trear all js code as newer version
+// alert(3+3)  // we are using node.js not browser that is why it will show error.
+
+console.log(typeof null) //null is object
+console.log(typeof undefined); //undefined is undefined
+ 
---
+++ Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=14458;
+let accountEmail = "ababdullah216@gmail.com"
+var accountPassword = "abc123"
+accountCity = "Islamabad"
+
+
+console.log(accountId);
+// accountId = 3;
+accountEmail = "acac@ab.com"
+accountPassword = "a123"
+accountCity = "pakistan"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
